458fca701e7c55bb85b3b5409fdb7b1c26a7b9f9
Оксана <@OKSANA>
2016-12-05 09:00:34 +0300

    Added simple WcfCalculatorService

---
+++ Calculator/WcfCalculatorService/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WcfCalculatorService
+{
+    public class Calculator : ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Sqrt(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
